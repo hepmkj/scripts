3e65bc1e7696a48b3aadab4e643570d88166bac6
hepmkj <hepmkj@gmail.com>
2020-07-03 16:23:33 +0000

    include minikube installation

--- install_ready_script.sh
+++ install_ready_script.sh
@@ -17,6 +17,43 @@ sudo systemctl enable docker
 
 }
 
+install_minikube () {
+#update 
+sudo yum -y update
+
+#Start by installing the required packages:
+
+sudo yum -y install epel-release
+
+sudo yum -y install libvirt qemu-kvm virt-install virt-top libguestfs-tools bridge-utils
+
+#Then, start and enable the libvirtd service:
+
+sudo systemctl start libvirtd
+
+sudo systemctl enable libvirtd
+
+#Confirm the virtualization service is running with the command:
+
+systemctl status libvirtd
+sudo usermod -a -G libvirt $(whoami)
+sudo systemctl restart libvirtd.service
+
+#Install minikube
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube-linux-amd64
+sudo mv minikube-linux-amd64 /usr/local/bin/minikube
+
+#Install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl  /usr/local/bin/
+
+#start  minikube
+minikube start
+
+}
+
 #install_git
 #install_docker
-
+install_minikube
